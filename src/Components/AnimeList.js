2277236a08f69890c702957c5fd07a99636672db
dejvi <dejviepadejvi@gmail.com>
2023-02-20 15:37:50 +0100

    rregulluam butonin addToList dhe RemoveFrom

--- src/Components/AnimeList.js
+++ src/Components/AnimeList.js
@@ -12,13 +12,15 @@ export const AnimeList = ({ animelist,setAnimeInfo,animeComponent,handleList}) =
                 <img src={anime.images.jpg.large_image_url} alt="animeImage" />
                 <div className="anime-info">
                   <h4>{anime.title}</h4>
-                  <div className="overlay" onClick={()=>handleList(anime)}>
+                  <div className="overlay" >
                       <h4>{anime.title_japanese}</h4>
                       <h3>SYNOPSIS</h3>
                       <div className="synopsis">
                         <p>{anime.synopsis}</p>
                       </div>
-                      <AddToList/>
+                      <div onClick={()=>handleList(anime)}>
+                      <AddToList />
+                      </div>
                   </div>
 
                 </div>
