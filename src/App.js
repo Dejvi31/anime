9de5dbb52fc889c80e23fb1e546327591b6bce66
dejvi <dejviepadejvi@gmail.com>
2023-02-20 13:31:13 +0100

    rregulluam pak designin

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import { AddToList } from "./Components/AddToList";
 import { RemoveFromList } from "./Components/RemoveFromList";
 function App() {
   const initialState = JSON.parse(localStorage.getItem("myAnimeList")) || []
-  const [search,setSearch]=useState('Naruto')
+  const [search,setSearch]=useState('')
   const [animeData,setAnimeData]=useState();
   const [animeInfo,setAnimeInfo]=useState()
   const [myAnimeList,setMyAnimeList]=useState(initialState)
+  const [howMuch,setHowMuch] = useState(12)
+
+  const handleHowMuch = () => {
+    setHowMuch(howMuch + 3)
+  }
 
   useEffect(() => {
     localStorage.setItem("myAnimeList", JSON.stringify(myAnimeList))
@@ -34,17 +39,17 @@ function App() {
  
   useEffect(()=>{
     const getData=async()=>{
-      const res=await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=20`)
+      const res=await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=${howMuch}`)
       const resData= await res.json();
       setAnimeData(resData.data)
   }
     getData()
-  },[search])
+  },[search,howMuch])
 
   return (
     <>
         <div className="header">
-          <h1>MyAnimeList</h1>
+          <h1 onClick={() => setSearch('')} style={{cursor: 'pointer'}}>MyAnimeList</h1>
           <div className="search-box">
               <input type="search" placeholder="Add a space after you search your anime ..." 
               onChange={(e)=>setSearch(e.target.value)}/>
@@ -65,20 +70,24 @@ function App() {
                 handleList={(anime)=>addTo(anime)}
                 />
             </div>
-           
-        
-        
-        <h2 className="text-heading">My List</h2>
-            <div className="row">
+            <div className="btn">
+            <button onClick={handleHowMuch}>Load More</button>
+            </div>
+            </div>
+          
+            <div className="MY_LIST">
+            <h2 className="TEXT_HEADING">My List</h2>
+            <div className="ROW">
                 <AnimeList 
                 animelist={myAnimeList}
                 setAnimeInfo={setAnimeInfo}
                 animeComponent={RemoveFromList}
                 handleList={(anime)=>removeFrom(anime)}
                 />
-            </div>
-            </div>
-            </div>
+                </div>
+             </div>
+             </div>
+            
     </>
   );
 }
